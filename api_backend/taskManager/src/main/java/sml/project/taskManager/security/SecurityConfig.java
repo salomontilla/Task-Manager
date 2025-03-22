44eb82176c7e9c914559c773e88744a5257e2da0
salo <salomontilla@outlook.com>
2025-03-22 18:03:27 -0500

    add: password encoder

--- api_backend/taskManager/src/main/java/sml/project/taskManager/security/SecurityConfig.java
+++ api_backend/taskManager/src/main/java/sml/project/taskManager/security/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,5 +24,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
     }
 }
