7e62e6631ce27c2f25d323958316198649921754
salo <salomontilla@outlook.com>
2025-03-22 18:03:10 -0500

    create: details user class

---
+++ api_backend/taskManager/src/main/java/sml/project/taskManager/models/DetailsUser.java
@@ -0,0 +1,27 @@
+package sml.project.taskManager.models;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@AllArgsConstructor
+public class DetailsUser implements UserDetails {
+    private User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+}
