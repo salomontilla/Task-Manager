e65efe02b62eca427233ca3035b8191a04623d04
salo <salomontilla@outlook.com>
2025-03-22 17:40:02 -0500

    add: create endpoints

--- api_backend/taskManager/src/main/java/sml/project/taskManager/controllers/AuthController.java
+++ api_backend/taskManager/src/main/java/sml/project/taskManager/controllers/AuthController.java
@@ -1,10 +1,17 @@
 package sml.project.taskManager.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AuthController {
-
+    //Este método se encarga de autenticar al usuario
+    @PostMapping("/register")
+    public String registerUser() {
+        return "Register user";
+    }
+    @PostMapping("/login")
+    public String loginUser() {
+        return "Login user";
+    }
 }
