d69e33fb92a1c72295e3ec2d4a52b10716e00045
salo <salomontilla@outlook.com>
2025-03-27 21:39:49 -0500

    feat: generate jwt correctly, login api done

--- api_backend/taskManager/src/main/java/sml/project/taskManager/controllers/AuthController.java
+++ api_backend/taskManager/src/main/java/sml/project/taskManager/controllers/AuthController.java
@@ -1,17 +1,35 @@
 package sml.project.taskManager.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+import sml.project.taskManager.models.DetailsUser;
+import sml.project.taskManager.models.JwtDto;
+import sml.project.taskManager.models.User;
+import sml.project.taskManager.models.UserDTO;
+import sml.project.taskManager.security.JwtUtil;
+
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    //Este método se encarga de autenticar al usuario
+    private final AuthenticationManager authManager;
+    private final JwtUtil jwtService;
+
     @PostMapping("/register")
     public String registerUser() {
         return "Register user";
     }
     @PostMapping("/login")
-    public String loginUser() {
-        return "Login user";
+    public ResponseEntity<JwtDto> loginUser(@RequestBody UserDTO userDto) {
+        Authentication auth = new UsernamePasswordAuthenticationToken(userDto.username(), userDto.password());
+        Authentication authUser = authManager.authenticate(auth);
+        String token = jwtService.generateToken((DetailsUser) authUser.getPrincipal());
+        return ResponseEntity.ok(new JwtDto(token));
     }
 }
--- api_backend/taskManager/src/main/java/sml/project/taskManager/models/DetailsUser.java
+++ api_backend/taskManager/src/main/java/sml/project/taskManager/models/DetailsUser.java
@@ -1,7 +1,9 @@
 package sml.project.taskManager.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,11 +11,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class DetailsUser implements UserDetails {
     public DetailsUser(User user) {
         this.user = user;
     }
-
+    private long id;
+    private String nombre;
+    private String clave;
     private User user;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
